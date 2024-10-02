55ed91e876d55609733ed359a3d96f2aceb04b46
hzumbado <116983628+hzumbado@users.noreply.github.com>
2024-10-01 22:44:28 -0400

    silence gbif user

--- gbif_user.R
+++ gbif_user.R
@@ -6,6 +6,6 @@ rm(list = ls())
 install.packages('usethis')
 usethis::edit_r_environ()
 
-GBIF_USER = '' # GBIF username
-GBIF_PWD = '' # GBIF password
-GBIF_EMAIL = '' # GBIF email
+#GBIF_USER = '' # GBIF username
+#GBIF_PWD = '' # GBIF password
+#GBIF_EMAIL = '' # GBIF email
